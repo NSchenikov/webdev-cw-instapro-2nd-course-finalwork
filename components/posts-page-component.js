68c25c8c0cd9ae9f644d2ba06fec2a4e74bda928
Nikita-Schenikov <nikita-shenikov@mail.ru>
2023-06-24 23:33:29 +0300

    likes on users posts page now working

--- components/posts-page-component.js
+++ components/posts-page-component.js
@@ -1,81 +1,54 @@
 import { POSTS_PAGE, USER_POSTS_PAGE } from "../routes.js";
 import { renderHeaderComponent } from "./header-component.js";
-import { posts, goToPage, getToken, renderApp } from "../index.js";
-import { getPosts, putDislike, putLike } from "../api.js";
+import { posts, goToPage, getToken, renderApp} from "../index.js";
+import { getPosts, putDislike, putLike, userPsts } from "../api.js";
+import { renderUserPostsPageComponent } from "./user-posts-page-component.js";
+
+export function initLikes(arr, rendering) {
+  for(let postLike of document.querySelectorAll(".like-button")) {
+    let index = postLike.dataset.index;
+    postLike.addEventListener("click", () => {
+      console.log(`уже лайкнут? ${postLike.dataset.isliked}`);
+      if(postLike.dataset.isliked === 'true') {
+        putDislike({
+          token: getToken(),
+          id: postLike.dataset.postId,
+        })
+        .then(() => {
+          getPosts({ getToken });
+          arr[index].isLiked = false; 
+          arr[index].likes.forEach((user, indx) => {
+            // console.log(user.id);
+            if(user.id === JSON.parse(window.localStorage.getItem("user"))._id) { //берем залогиненого пользователя, которого запомнил браузер после авторизации
+              arr[index].likes.splice(indx, 1); //удаляем из локального объекта постов лайк текущего пользователя
+            }
+          });
+          // console.log(psts);
+        })
+        .then(() => {
+          return rendering();
+        });
+      } else if(postLike.dataset.isliked === 'false') {
+        putLike({
+          token: getToken(),
+          id: postLike.dataset.postId,
+        })
+        .then(() => {
+          getPosts({ getToken });
+          arr[index].isLiked = true;
+          arr[index].likes.push({id: JSON.parse(window.localStorage.getItem("user"))._id, name: JSON.parse(window.localStorage.getItem("user")).name}); //добавляем сохраненного браузером пользователя в локальный массив постов
+        })
+        .then(() => {
+          return rendering();
+        });
+        // console.log(psts);
+      }
+    });
+  }
+}
 
 export function renderPostsPageComponent({ appEl, psts }) {
   // TODO: реализовать рендер постов из api +
-
-  function initLikes() {
-    for(let postLike of document.querySelectorAll(".like-button")) {
-      let index = postLike.dataset.index;
-      postLike.addEventListener("click", () => {
-        console.log(`уже лайкнут? ${postLike.dataset.isliked}`);
-        if(postLike.dataset.isliked === 'true') {
-          putDislike({
-            token: getToken(),
-            id: postLike.dataset.postId,
-          })
-          .then(() => {
-            getPosts({ getToken });
-            psts[index].isLiked = false; 
-            psts[index].likes.forEach((user, indx) => {
-              // console.log(user.id);
-              if(user.id === JSON.parse(window.localStorage.getItem("user"))._id) { //берем залогиненого пользователя, которого запомнил браузер после авторизации
-                psts[index].likes.splice(indx, 1); //удаляем из локального объекта постов лайк текущего пользователя
-              }
-            });
-            // console.log(psts);
-          })
-          .then(() => {
-            return rendering();
-          });
-        } else if(postLike.dataset.isliked === 'false') {
-          putLike({
-            token: getToken(),
-            id: postLike.dataset.postId,
-          })
-          .then(() => {
-            getPosts({ getToken });
-            psts[index].isLiked = true;
-            psts[index].likes.push({id: JSON.parse(window.localStorage.getItem("user"))._id, name: JSON.parse(window.localStorage.getItem("user")).name}); //добавляем сохраненного браузером пользователя в локальный массив постов
-          })
-          .then(() => {
-            return rendering();
-          });
-          // console.log(psts);
-        }
-      });
-    }
-
-
-    // [...document.querySelectorAll(".like-button")].forEach((postLike, index) => {
-    //   postLike.addEventListener('click', () => {
-    //     if(psts[index].isLiked) {
-    //       putDislike({
-    //         token: getToken(),
-    //         id: postLike.dataset.postId,
-    //       })
-    //       .then(newPost => {
-    //         psts.splice(index, 1, newPost);
-    //         rendering();
-    //       })
-    //       .catch(error => alert(error.message));
-    //     } else {
-    //       putLike({
-    //         token: getToken(),
-    //         id: postLike.dataset.postId,
-    //       })
-    //       .then(newPost => {
-    //         psts.splice(index, 1, newPost);
-    //         rendering();
-    //       })
-    //       .catch(error => alert(error.message));
-    //     }
-    //   });
-    // });
-
-  }
 
   function rendering() {
       let pstsHtml = psts.map((pst, index) => {
@@ -89,7 +62,7 @@ export function renderPostsPageComponent({ appEl, psts }) {
             <img class="post-image" src=${pst.imageUrl}>
           </div>
           <div class="post-likes">
-            <button data-post-id=${pst.id} data-isLiked=${pst.isLiked} data-index=${index} data-userId=${pst.userId} data-username=${pst.userName} class="like-button">
+            <button data-post-id=${pst.id} data-isLiked=${pst.isLiked} data-index=${index} class="like-button">
               <img src="./assets/images/${pst.isLiked ? "like-active" : "like-not-active"}.svg">
             </button>
             <p class="post-likes-text">
@@ -124,8 +97,18 @@ export function renderPostsPageComponent({ appEl, psts }) {
       renderHeaderComponent({
         element: document.querySelector(".header-container"),
       });
-      initLikes();
-  };
+
+      initLikes(psts, rendering);
+
+      for (let userEl of document.querySelectorAll(".post-header")) {
+        userEl.addEventListener("click", () => {
+          renderUserPostsPageComponent({appEl, userPsts});
+          goToPage(USER_POSTS_PAGE, {
+            userId: userEl.dataset.userId,
+          });
+        });
+      }
+  }
 
 
   rendering();
